c26c451dd52cd5b5148e0f881758f3217e9a292b
natalyamn <161460223+natalyamn@users.noreply.github.com>
2024-03-19 10:50:13 +0100

    Add files via upload

--- 01. Sprint 1 SQL/Sprint1_NM.sql
+++ 01. Sprint 1 SQL/Sprint1_NM.sql
@@ -17,7 +17,7 @@ ORDER BY nombre_compañia;
 #Desde la sección de marketing te solicitan que les pases un listado de los países que están realizando compras.
 SELECT DISTINCT country AS pais
 FROM transactions.company
-ORDER BY country;
+ORDER BY pais;
 
 -- Ejercicio 4
 #Desde marketing también quieren saber desde cuántos países se realizan las compras.
@@ -36,10 +36,10 @@ SELECT 	t.company_id AS identificador,
 		c.company_name AS nombre_compañia, 
         round(AVG(t.amount),2) AS gasto_promedio
 FROM transactions.transaction AS t
-	JOIN transactions.company AS c	
+JOIN transactions.company AS c	
 	ON t.company_id=c.id
 WHERE declined=0 
-GROUP BY t.company_id
+GROUP BY identificador
 ORDER BY gasto_promedio DESC
 LIMIT 1;
 
@@ -58,22 +58,22 @@ FROM transactions.transaction;
 
 -- Ejercicio 2
 #¿En qué día se realizaron las cinco ventas más costosas? Muestra la fecha de la transacción y la sumatoria de la cantidad de dinero.
-SELECT DATE(timestamp) AS 'Fecha transacción',
-	SUM(amount) AS 'cantidad de dinero'
+SELECT DATE(timestamp) Fecha_transacción,
+	SUM(amount) cantidad_dinero
 FROM transactions.transaction
 WHERE declined=0
-GROUP BY DATE(timestamp)
-ORDER BY SUM(amount) DESC
+GROUP BY Fecha_transacción
+ORDER BY cantidad_dinero DESC
 LIMIT 5;
 
 -- Ejercicio 3
 #¿En qué día se realizaron las cinco ventas de menor valor? Muestra la fecha de la transacción y la sumatoria de la cantidad de dinero. 
-SELECT DATE(timestamp) AS 'Fecha transacción',
-		SUM(amount) AS 'cantidad de dinero'
+SELECT DATE(timestamp) AS Fecha_transacción,
+		SUM(amount) AS cantidad_dinero
 FROM transactions.transaction
 WHERE declined=0
-GROUP BY DATE(timestamp)
-ORDER BY SUM(amount) ASC
+GROUP BY Fecha_transacción
+ORDER BY cantidad_dinero ASC
 LIMIT 5;
 
 -- Ejercicio 4
@@ -81,7 +81,7 @@ LIMIT 5;
 SELECT country AS pais, 
 		round(AVG(amount),2) AS gasto_promedio
 FROM transactions.company AS c
-	JOIN transactions.transaction AS t
+JOIN transactions.transaction AS t
 	ON c.id=t.company_id
 WHERE declined=0
 GROUP BY pais
@@ -96,13 +96,12 @@ entre 100 y 200 euros. Ordena los resultados de mayor a menor cantidad gastada. 
 SELECT 	company_name AS nombre_empresa, 
         phone AS telefono, 
         country AS pais,
-        SUM(amount) AS gastos
+        amount AS gasto
 FROM transactions.company AS c
-	JOIN transactions.transaction AS t
+JOIN transactions.transaction AS t
     ON c.id=t.company_id
 WHERE declined=0 AND amount BETWEEN 100 AND 200
-GROUP BY c.id
-ORDER BY gastos DESC;
+ORDER BY gasto DESC;
 
 -- Ejercicio 2
 #Indica el nombre de las compañías que realizaron compras el 16 de marzo de 2022, 28 de febrero de 2022 y 13 de febrero de 2022.
@@ -112,7 +111,7 @@ SELECT c.id AS identificador,
         amount AS gasto, 
         timestamp AS Fecha_hora_transaccion
 FROM transactions.company AS c
-	JOIN transactions.transaction AS t
+JOIN transactions.transaction AS t
     ON c.id=t.company_id
 WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13')
 ORDER BY Fecha_hora_transaccion;
@@ -121,7 +120,7 @@ ORDER BY Fecha_hora_transaccion;
 SELECT c.id AS identificador, 
 		company_name AS nombre_compañia 
 FROM transactions.company AS c
-	JOIN transactions.transaction AS t
+JOIN transactions.transaction AS t
     ON c.id=t.company_id
 WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13')
 GROUP BY identificador;
@@ -131,6 +130,6 @@ GROUP BY identificador;
 SELECT DISTINCT (c.id) AS identificador, 
 		company_name AS nombre_compañia 
 FROM transactions.company AS c
-	JOIN transactions.transaction AS t
+JOIN transactions.transaction AS t
     ON c.id=t.company_id
 WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13');
