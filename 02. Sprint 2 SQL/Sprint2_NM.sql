0f79ed10545f7f03a95748500b071a547535d700
natalyamn <161460223+natalyamn@users.noreply.github.com>
2024-04-08 16:47:39 +0200

    Add files via upload

--- 02. Sprint 2 SQL/Sprint2_NM.sql
+++ 02. Sprint 2 SQL/Sprint2_NM.sql
@@ -2,102 +2,44 @@
 
 -- Ejercicio 1
 #Muestra todas las transacciones realizadas por empresas de Alemania. 
--- método 1: con subquery 
-#subquery: 
-SELECT id
-FROM transactions.company
-WHERE country='Germany';
-
-#query principal
 SELECT *
-FROM transactions.transaction
+FROM transactions_2.transaction
 WHERE company_id IN (SELECT id
 					FROM transactions.company
 					WHERE country='Germany');
-
--- método 2: con JOIN
-SELECT t.*
-FROM transactions.transaction t
-JOIN transactions.company c
-    ON t.company_id=c.id
-WHERE country='Germany';
 
 
 -- Ejercicio 2
 /*Marketing está preparando algunos informes de cierres de gestión, te piden que les pases un listado de las empresas que han realizado 
 transacciones por una suma superior a la media de todas las transacciones.*/
--- subquery 1: 
-SELECT ROUND(AVG(amount),2)
-FROM transactions.transaction
-WHERE declined=0;
--- subquery 2: 
-SELECT company_id, amount
-FROM transactions.transaction
-WHERE declined=0 AND amount > (SELECT ROUND(AVG(amount),2)
-								FROM transactions.transaction
-								WHERE declined=0);
-
--- método 1: query principal con subconsultas en la cláusula JOIN:
-SELECT c.*
-FROM transactions.company AS c
-JOIN (SELECT company_id, amount
-		FROM transactions.transaction
-		WHERE declined=0 AND amount > (SELECT ROUND(AVG(amount),2)
-								FROM transactions.transaction
-								WHERE declined=0)
-		) AS comp_trans_sup
-ON c.id=comp_trans_sup.company_id
-GROUP BY company_id
-ORDER BY company_id;
-
--- método 2: query principal con subconsulta en la cláusula HAVING:
-SELECT c.*
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
-ON c.id=t.company_id
-WHERE declined=0 AND amount > (SELECT ROUND(AVG(amount),2)
-								FROM transactions.transaction
-								WHERE declined=0)
-GROUP BY company_id
-ORDER BY company_id;
+SELECT *
+FROM transactions_2.company
+WHERE id IN (SELECT company_id
+			FROM transactions_2.transaction
+			WHERE amount > (
+							SELECT AVG(amount)
+							FROM transactions_2.transaction
+                            )
+			);
 
 
 -- Ejercicio 3
 /*El departamento de contabilidad perdió la información de las transacciones realizadas por una empresa, pero no recuerdan su nombre, 
 sólo recuerdan que su nombre iniciaba con la letra c. ¿Cómo puedes ayudarles? Coméntelo acompañándolo de la información de las transacciones.*/
--- método 1: con subquery
-#subquery:
-SELECT id, company_name
-FROM transactions.company
-WHERE company_name LIKE 'c%';
+SELECT company_c.company_name, t.* 
+FROM transactions_2.transaction t, 
+	(SELECT id, company_name
+	FROM transactions_2.company
+	WHERE company_name LIKE 'c%') company_c
+WHERE t.company_id=company_c.id;
 
-#query principal 
-SELECT compañias_c.company_name AS nombre_compañia, t.* 
-FROM transactions.transaction t
-JOIN (SELECT id, company_name
-	FROM transactions.company
-	WHERE company_name LIKE 'c%') compañias_c
-ON t.company_id=compañias_c.id;
-
--- método 2: con JOIN
-SELECT c.company_name AS nombre_compañia, t.* 
-FROM transactions.company c
-JOIN transactions.transaction t
-ON c.id=t.company_id
-WHERE company_name LIKE 'c%';
-
-    
+     
 -- Ejercicio 4
 #Eliminaron del sistema a las empresas que no tienen transacciones registradas, entrega el listado de estas empresas.
--- subquery:
-SELECT DISTINCT company_id
-FROM transactions.transaction;
-
--- query principal:
 SELECT id, company_name
-FROM transactions.company c
+FROM transactions_2.company c
 WHERE NOT EXISTS (SELECT DISTINCT company_id
-				FROM transactions.transaction t
+				FROM transactions_2.transaction t
                 WHERE c.id=t.company_id);
 
 
@@ -107,55 +49,22 @@ WHERE NOT EXISTS (SELECT DISTINCT company_id
 -- Ejercicio 1
 /*En tu empresa, se plantea un nuevo proyecto para lanzar algunas campañas publicitarias para hacer competencia a la compañía Non Institute. 
 Para ello, te piden la lista de todas las transacciones realizadas por empresas que están ubicadas en el mismo país que esta compañía.*/
--- subquery: 
-SELECT country
-FROM transactions.company
-WHERE company_name='Non Institute';
-
--- query principal:
-SELECT company_name AS nombre_compañia, t.*
-FROM transactions.transaction t
-JOIN transactions.company c
-	ON t.company_id=c.id
-WHERE c.country=(SELECT country
-				FROM transactions.company
-				WHERE company_name='Non Institute');
+SELECT c.company_name, t.*
+FROM transactions_2.transaction t, 
+	transactions_2.company c
+WHERE t.company_id=c.id AND country=(SELECT country
+									FROM transactions_2.company
+									WHERE company_name='Non Institute');
                 
 
 -- Ejercicio 2
 #El departamento de contabilidad necesita que encuentres a la empresa que ha realizado la transacción de mayor suma en la base de datos.
--- subquery:
-SELECT company_id, SUM(amount) AS suma_transaccion
-FROM transactions.transaction
-WHERE declined=0
-GROUP BY company_id
-ORDER BY suma_transaccion DESC
-LIMIT 1;
-
--- query principal con subquery en JOIN
-SELECT c.*, sum_trans.suma_transaccion
-FROM transactions.company AS c
-JOIN (SELECT company_id, SUM(amount) AS suma_transaccion
-		FROM transactions.transaction
-		WHERE declined=0
-		GROUP BY company_id
-		ORDER BY suma_transaccion DESC
-		LIMIT 1) AS sum_trans
-ON c.id=sum_trans.company_id;
-
--- query principal con subquery en HAVING
-SELECT c.*, SUM(amount) AS suma_transaccion
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
-	ON c.id=t.company_id
-WHERE declined=0
-GROUP BY company_id
-HAVING suma_transaccion=(SELECT SUM(amount) 
-						FROM transactions.transaction
-						WHERE declined=0
-						GROUP BY company_id
-						ORDER BY SUM(amount) DESC
-						LIMIT 1);
+SELECT *
+FROM transactions_2.company
+WHERE id = (SELECT company_id
+			FROM transactions_2.transaction
+			ORDER BY amount DESC
+			LIMIT 1);
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -164,86 +73,32 @@ HAVING suma_transaccion=(SELECT SUM(amount)
 -- Ejercicio 1
 /*Se están estableciendo los objetivos de la empresa para el siguiente trimestre, por lo que necesitan una sólida base para evaluar el rendimiento y 
 medir el éxito en los diferentes mercados. Para ello, necesitan el listado de los países cuyo promedio de transacciones sea superior al promedio general.*/
--- subquery:
-SELECT round(AVG(amount),2) AS promedio_general
-FROM transactions.transaction
-WHERE declined=0;
-
--- query principal: 
 SELECT c.country AS pais, 
 		round(AVG(amount),2) AS promedio_transacciones
-FROM transactions.company c 
-JOIN transactions.transaction t 
-    ON c.id=t.company_id
-WHERE declined=0
+FROM transactions_2.company c, 
+	transactions_2.transaction t 
+WHERE c.id=t.company_id
 GROUP BY pais
-HAVING promedio_transacciones > (SELECT round(AVG(amount),2)
-								FROM transactions.transaction
-								WHERE declined=0);
+HAVING promedio_transacciones > (SELECT AVG(amount)
+								FROM transactions_2.transaction);
 
 
 -- Ejercicio 2
 /*Necesitamos optimizar la asignación de los recursos y dependerá de la capacidad operativa que se requiera, por lo que te piden la información sobre 
 la cantidad de transacciones que realizan las empresas, pero el departamento de recursos humanos es exigente y quiere un listado de las empresas donde 
 especifiques si tienen más de 4 o menos transacciones.*/
--- operaciones aceptadas
-SELECT company_id, COUNT(*) AS aceptadas
-FROM transactions.transaction t
-WHERE declined=0 
-GROUP BY company_id;
-
--- operaciones rechazadas
-SELECT company_id, COUNT(*) AS rechazadas
-FROM transactions.transaction t
-WHERE declined=1
-GROUP BY company_id;
-
--- query principal
-SELECT 	c.company_name AS nombre_compañia,
-		t_acep.company_id AS identificador_compañia, 
-		t_acep.aceptadas AS transacciones_aceptadas, 
-		IFNULL(t_rech.rechazadas,0) AS transacciones_rechazadas, 
+SELECT 	company_name AS nombre_compañia,
+		company_id AS identificador_compañia,
+		SUM(CASE WHEN declined=0 THEN 1 ELSE 0 END) AS transacciones_aceptadas,
+		IFNULL(SUM(CASE WHEN declined=1 THEN 1 END),0) AS transacciones_rechazadas,
+		COUNT(*) AS total_transacciones, 
         CASE
-			WHEN (t_acep.aceptadas + IFNULL(t_rech.rechazadas,0)) < 4 THEN 'Menos de 4 transacciones'
+			WHEN COUNT(*) < 4 THEN 'Menos de 4 transacciones'
+            WHEN COUNT(*) = 4 THEN '4 transacciones'
             ELSE 'Más de 4 transacciones'
-		END AS cantidad_transacciones
-FROM (SELECT company_id, COUNT(*) AS aceptadas
-		FROM transactions.transaction 
-		WHERE declined=0 
-		GROUP BY company_id) t_acep
-LEFT JOIN (SELECT company_id, COUNT(*) AS rechazadas
-			FROM transactions.transaction 
-			WHERE declined=1 
-			GROUP BY company_id) t_rech
-	ON t_acep.company_id=t_rech.company_id
-JOIN transactions.company c
-	ON t_acep.company_id=c.id
-ORDER BY transacciones_aceptadas DESC, identificador_compañia;
-
-
--- metodo 2: definiendo las tablas de operaciones aceptadas y rechazadas al inicio y haciendo la consulta a continuación
-WITH 
-t_acep AS 
-(SELECT company_id, COUNT(*) AS aceptadas
-FROM transactions.transaction t
-WHERE declined=0 
-GROUP BY company_id), 
-t_rech AS 
-(SELECT company_id, COUNT(*) AS rechazadas
-FROM transactions.transaction t
-WHERE declined=1
-GROUP BY company_id)
-SELECT 	c.company_name AS nombre_compañia,
-		t_acep.company_id AS identificador_compañia, 
-		t_acep.aceptadas AS transacciones_aceptadas, 
-		IFNULL(t_rech.rechazadas,0) AS transacciones_rechazadas, 
-        CASE
-			WHEN (t_acep.aceptadas + IFNULL(t_rech.rechazadas,0)) < 4 THEN 'Menos de 4 transacciones'
-            ELSE 'Más de 4 transacciones'
-		END AS cantidad_transacciones
-FROM t_acep
-LEFT JOIN t_rech
-	ON t_acep.company_id=t_rech.company_id
-JOIN transactions.company c
-	ON t_acep.company_id=c.id
-ORDER BY transacciones_aceptadas DESC, identificador_compañia;
+		END AS descripción_transacciones
+FROM transactions_2.transaction t, 
+	transactions_2.company c
+WHERE t.company_id=c.id
+GROUP BY identificador_compañia
+ORDER BY total_transacciones DESC, identificador_compañia;
