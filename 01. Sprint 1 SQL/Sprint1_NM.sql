aba50d3044c1cc7b02a6f4e7736b90dbafe5cbd9
natalyamn <161460223+natalyamn@users.noreply.github.com>
2024-04-15 20:16:09 +0200

    Add files via upload

--- 01. Sprint 1 SQL/Sprint1_NM.sql
+++ 01. Sprint 1 SQL/Sprint1_NM.sql
@@ -1,3 +1,5 @@
+USE transactions_2;
+
 /* NIVEL 1 */
 
 -- Ejercicio 1
@@ -7,39 +9,39 @@ Asegúrate de incluir un diagrama que ilustre la relación entre las distintas t
 
 -- Ejercicio 2
 #Realiza la siguiente consulta: Tienes que obtener el nombre, email y país de cada compañía, ordena los datos en función del nombre de las compañías.
-SELECT company_name AS nombre_compañia, 
+SELECT 	company_name, 
 		email, 
-        country AS pais
-FROM transactions.company
-ORDER BY nombre_compañia;
+        country
+FROM company
+ORDER BY company_name;
 
 -- Ejercicio 3
 #Desde la sección de marketing te solicitan que les pases un listado de los países que están realizando compras.
 SELECT DISTINCT country AS pais
-FROM transactions.company
+FROM company
 ORDER BY pais;
 
 -- Ejercicio 4
 #Desde marketing también quieren saber desde cuántos países se realizan las compras.
 SELECT COUNT(DISTINCT country) AS cantidad_paises
-FROM transactions.company;
+FROM company;
 
 -- Ejercicio 5
 #Tu jefe identifica un error con la compañía que tiene identificador 'b-2354'. Por tanto, te solicita que le indiques el país y nombre de compañía de este identificador. 
-SELECT country AS pais, company_name AS nombre_compañia
-FROM transactions.company
+SELECT country, company_name
+FROM company
 WHERE id='b-2354';
 
 -- Ejercicio 6
 #Además, tu jefe te solicita que indiques ¿cuál es la compañía con mayor gasto medio? 
-SELECT 	t.company_id AS identificador, 
-		c.company_name AS nombre_compañia, 
+SELECT 	company_id, 
+		company_name, 
         round(AVG(t.amount),2) AS gasto_promedio
-FROM transactions.transaction AS t
-JOIN transactions.company AS c	
+FROM transaction t
+JOIN company c	
 	ON t.company_id=c.id
 WHERE declined=0 
-GROUP BY identificador
+GROUP BY company_id
 ORDER BY gasto_promedio DESC
 LIMIT 1;
 
@@ -50,17 +52,17 @@ LIMIT 1;
 /* Tu jefe está redactando un informe de cierre del año y te solicita que le envíes información relevante para el documento. 
 Para ello te solicita verificar si en la base de datos existen compañías con identificadores (id) duplicados.*/
 SELECT COUNT(id) - COUNT(DISTINCT id) AS 'cantidad compañias duplicadas'
-FROM transactions.company;
+FROM company;
 
-SELECT COUNT(company_id) 'cantidad total de compañias', 
+SELECT COUNT(company_id) AS 'cantidad total de compañias', 
 	COUNT(DISTINCT company_id) AS 'cantidad de compañias unicas'
-FROM transactions.transaction; 
+FROM transaction; 
 
 -- Ejercicio 2
 #¿En qué día se realizaron las cinco ventas más costosas? Muestra la fecha de la transacción y la sumatoria de la cantidad de dinero.
-SELECT DATE(timestamp) Fecha_transacción,
-	SUM(amount) cantidad_dinero
-FROM transactions.transaction
+SELECT DATE(timestamp) AS Fecha_transacción,
+		SUM(amount) AS cantidad_dinero
+FROM transaction
 WHERE declined=0
 GROUP BY Fecha_transacción
 ORDER BY cantidad_dinero DESC
@@ -70,7 +72,7 @@ LIMIT 5;
 #¿En qué día se realizaron las cinco ventas de menor valor? Muestra la fecha de la transacción y la sumatoria de la cantidad de dinero. 
 SELECT DATE(timestamp) AS Fecha_transacción,
 		SUM(amount) AS cantidad_dinero
-FROM transactions.transaction
+FROM transaction
 WHERE declined=0
 GROUP BY Fecha_transacción
 ORDER BY cantidad_dinero ASC
@@ -78,10 +80,10 @@ LIMIT 5;
 
 -- Ejercicio 4
 #¿Cuál es el promedio de gasto por país? Presenta los resultados ordenados de mayor a menor promedio.
-SELECT country AS pais, 
-		round(AVG(amount),2) AS gasto_promedio
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
+SELECT 	country AS pais, 
+		ROUND(AVG(amount),2) AS gasto_promedio
+FROM company c
+JOIN transaction t
 	ON c.id=t.company_id
 WHERE declined=0
 GROUP BY pais
@@ -93,43 +95,34 @@ ORDER BY gasto_promedio DESC;
 -- Ejercicio 1
 /* Presenta el nombre, teléfono y país de las compañías, junto con la cantidad total gastada, de aquellas que realizaron transacciones con un gasto comprendido 
 entre 100 y 200 euros. Ordena los resultados de mayor a menor cantidad gastada. */
-SELECT 	company_name AS nombre_empresa, 
-        phone AS telefono, 
-        country AS pais,
-        amount AS gasto
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
+SELECT 	company_name, 
+        phone, 
+        country,
+        amount
+FROM company c
+JOIN transaction t
     ON c.id=t.company_id
 WHERE declined=0 AND amount BETWEEN 100 AND 200
-ORDER BY gasto DESC;
+ORDER BY amount DESC;
 
 -- Ejercicio 2
 #Indica el nombre de las compañías que realizaron compras el 16 de marzo de 2022, 28 de febrero de 2022 y 13 de febrero de 2022.
 -- Nombre de las compañias y detalles sobre las transacciones efectuadas en las fechas indicadas
-SELECT c.id AS identificador, 
-		company_name AS nombre_compañia, 
-        amount AS gasto, 
+SELECT 	company_id, 
+		company_name, 
+        amount, 
         timestamp AS Fecha_hora_transaccion
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
+FROM company c
+JOIN transaction t
     ON c.id=t.company_id
 WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13')
 ORDER BY Fecha_hora_transaccion;
 
 -- Nombre e identificativo de las compañias 
-SELECT c.id AS identificador, 
-		company_name AS nombre_compañia 
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
+SELECT 	company_id, 
+		company_name
+FROM company c
+JOIN transaction t
     ON c.id=t.company_id
 WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13')
-GROUP BY identificador;
-
--- ó 
-
-SELECT DISTINCT (c.id) AS identificador, 
-		company_name AS nombre_compañia 
-FROM transactions.company AS c
-JOIN transactions.transaction AS t
-    ON c.id=t.company_id
-WHERE DATE(timestamp) IN ('2022-03-16', '2022-02-28', '2022-02-13');
+GROUP BY company_id;
