79b9658a8f426da36d335fed667c23a14915e86f
natalyamn <161460223+natalyamn@users.noreply.github.com>
2024-04-16 17:51:23 +0200

    Add files via upload

--- 03. Sprint 3 SQL/Sprint3_NM.sql
+++ 03. Sprint 3 SQL/Sprint3_NM.sql
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS credit_card (
     expiring_date VARCHAR(100)
 );
 
+ALTER TABLE credit_card
+MODIFY COLUMN cvv VARCHAR(3);
+
 -- hacemos modificaciones en la tabla de transacciones: creamos indices para optimizar las busquedas, creamos foreign keys
 CREATE INDEX idx_credit_card_id ON transaction(credit_card_id);
 ALTER TABLE transaction
 ADD FOREIGN KEY (credit_card_id) REFERENCES credit_card(id);
 
-CREATE INDEX idx_user_id ON transaction(user_id);
-ALTER TABLE transaction
-ADD FOREIGN KEY (user_id) REFERENCES user(id);
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- expiring_date de la tabla credit_card en formato varchar para poder cargar los datos; modificamos la fecha string a DATE (YYYY-MM-DD)
 # desactivamos el 'guardado' automatico para poder retroceder con ROLLBACK en caso de necesitarlo
@@ -45,19 +45,11 @@ SET AUTOCOMMIT = ON;
 #modificamos el formato de varchar a date
 ALTER TABLE credit_card
 MODIFY expiring_date DATE;
----------------------------------------------------------------------------
--- birth_date de la tabla user en formato varchar; lo modificamos a DATE
-UPDATE user
-SET birth_date=STR_TO_DATE(birth_date, '%b %d, %Y')
-WHERE id > 0;
 
-ALTER TABLE user
-MODIFY birth_date DATE;
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 /* Ejercicio 2. El departamento de Recursos Humanos ha identificado un error en el número de cuenta del usuario con ID CcU-2938. 
 Se requiere actualizar la información que identifica una cuenta bancaria a nivel internacional (identificado como "IBAN"): TR323456312213576817699999. 
 */
--- antes del cambio: IBAN TR301950312213576817638661
 UPDATE credit_card
 SET iban = 'TR323456312213576817699999'
 WHERE id= 'CcU-2938';
@@ -68,21 +60,32 @@ WHERE id='CcU-2938';
 
 
 #Ejercicio 3. En la tabla "transaction" ingresa un nuevo usuario con la información especificada. 
+/*Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails */
+#Añadimos el identificador de compañía en company, identificador de tarjeta en credit_card, identificador de usuario en user
+INSERT INTO company (id)
+VALUES ('b-9999');
+
+INSERT INTO credit_card (id)
+VALUES ('CcU-9999');
+
+INSERT INTO user (id)
+VALUES ('9999');
+
+#Ya no habría problemas para cargar los datos en la tabla de transacciones
 INSERT INTO transaction (id, credit_card_id, company_id, user_id, lat, longitude, amount, declined)
 VALUES ('108B1D1D-5B23-A76C-55EF-C568E49A99DD', 'CcU-9999', 'b-9999', '9999', '829.999', '-117.999', '111.11', '0');
-/*Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails */
-SET foreign_key_checks = 0;
-SET foreign_key_checks = 1;
 
 SELECT * 
 FROM transaction
 WHERE id='108B1D1D-5B23-A76C-55EF-C568E49A99DD';
 
+select * from TRANSACTION;
+
 #Ejercicio 4. Desde recursos humanos te solicitan eliminar la columna "pan" de la tabla credit_card. Recuerda mostrar el cambio realizado. 
 ALTER TABLE credit_card
 DROP COLUMN pan;
 
-SELECT * FROM credit_card;
+SHOW COLUMNS FROM credit_card;
 
 
 /* NIVEL 2 */
@@ -104,15 +107,15 @@ Presenta la vista creada, ordenando los datos de mayor a menor promedio de compr
 */
 CREATE VIEW VistaMarketing AS
 (
-SELECT 	company_name AS nombre_compañia,
-		phone AS telefono_contacto,
-		country AS pais_residencia,
-        compras.avg_compras AS promedio_compras
-FROM company c, 
-	(SELECT company_id, ROUND(AVG(amount),2) AS avg_compras
-	FROM transaction
-	GROUP BY company_id) compras
-WHERE compras.company_id=c.id
+SELECT 	c.company_name AS nombre_compañia,
+		c.phone AS telefono_contacto,
+		c.country AS pais_residencia,
+        ROUND(AVG(t.amount),2) AS promedio_compras
+FROM company c
+JOIN transaction t 
+ON c.id=t.company_id
+WHERE declined=0
+GROUP BY c.id
 );
 
 SELECT * 
@@ -123,10 +126,23 @@ ORDER BY promedio_compras DESC;
 #Ejercicio 3. Filtra la vista VistaMarketing para mostrar sólo las compañías que tienen su país de residencia en "Germany". 
 SELECT * 
 FROM VistaMarketing 
-WHERE pais_residencia = 'Germany';
+WHERE pais_residencia = 'Germany'
+ORDER BY promedio_compras DESC; 
 
 
 /* NIVEL 3 */
+
+CREATE INDEX idx_user_id ON transaction(user_id);
+ALTER TABLE transaction
+ADD FOREIGN KEY (user_id) REFERENCES user(id);
+---------------------------------------------------------------------------
+-- birth_date de la tabla user en formato varchar; lo modificamos a DATE
+UPDATE user
+SET birth_date=STR_TO_DATE(birth_date, '%b %d, %Y')
+WHERE id > 0;
+
+ALTER TABLE user
+MODIFY birth_date DATE;
 
 /* Ejercicio 1. La próxima semana tendrás una nueva reunión con los gerentes de marketing. Un compañero de tu equipo realizó modificaciones en la base de datos, 
 pero no recuerda cómo las realizó. Te pide que le ayudes a dejar los comandos ejecutados para obtener las modificaciones. 
@@ -145,11 +161,18 @@ ADD COLUMN fecha_actual DATE;
 
 UPDATE credit_card
 SET fecha_actual = CURRENT_DATE()
-WHERE id BETWEEN 'CcU-2938' AND 'CcU-4856';
+WHERE id BETWEEN 'CcU-2938' AND 'CcU-9999';
 
 -- modificar tipo de dato de columna pin de la tabla credit_card
 ALTER TABLE credit_card
 MODIFY COLUMN pin VARCHAR(4);
+
+-- otros posibles cambios en la tabal credit_card
+ALTER TABLE credit_card
+MODIFY COLUMN id VARCHAR(20);
+
+ALTER TABLE credit_card
+MODIFY COLUMN iban VARCHAR(50);
 
 
 /* Ejercicio 2. La empresa también te solicita crear una vista llamada "InformeTecnico" que contenga la siguiente información: 
@@ -162,11 +185,11 @@ Asegúrate de incluir información relevante de las tablas y utiliza alias para 
 Muestra los resultados de la vista, ordena los resultados de forma descendente en función de la variable ID de transacción. */
 CREATE VIEW InformeTecnico AS
 (
-SELECT t.id AS ID_transaccion, 
-		name AS Nombre_usuario, 
-        surname AS Apellido_usuario, 
-        iban AS IBAN_tarjeta,
-        company_name AS Nombre_compañia
+SELECT 	t.id AS ID_transaccion, 
+		u.name AS Nombre_usuario, 
+        u.surname AS Apellido_usuario, 
+        cred.iban AS IBAN_tarjeta,
+        c.company_name AS Nombre_compañia
 FROM transaction t
 JOIN user u
 	ON t.user_id=u.id
